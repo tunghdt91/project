dea66712b294f069546eb364cdaad77e0ac1c21b
tunghdt91 <tunghdt91@gmail.com>
2013-08-20 16:26:21 +0700

    dsfsd

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,5 +1,8 @@
 <div class='clear'></div>
 <?php $this->widget('bootstrap.widgets.TbAlert'); ?>
-<legend>
-    Welcome ! Index site
-</legend>
+<div class="alert alert-success">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <h4>Home pages</h4>
+  This is website demo CRUD using Yii Framework with Boostrap Css
+  <br/>
+</div>
