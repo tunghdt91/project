8b4e7ae954034c996dbe232e4ff7626b0ffa1776
tunghdt91 <tunghdt91@gmail.com>
2013-08-21 13:50:08 +0700

    fix current user

---
+++ protected/views/user/profile.php
@@ -0,0 +1,36 @@
+<legend>
+    <h3>My info</h3>
+</legend>
+<table class='detail-view table table-striped table-condensed' id='yw1'>
+    <tbody>
+        <tr class='odd '>
+            <th width="30%">Name :</th>
+            <td><?php echo $user->name; ?></td>
+     </tr>
+     <tr class='odd '>
+            <th>User Name</th>
+            <td><?php echo $user->username; ?></td>
+     </tr>
+     <tr class='odd '>
+            <th>Password :</th>
+            <td>****** (<a href="index.php?r=user/changePassword">Change</a>)</td>
+     </tr>
+     <tr class='odd '>
+            <th>Birthday</th>
+            <td><?php echo $user->birthday; ?></td>
+     </tr>
+      <tr class='odd '>
+            <th>Gender</th>
+            <td><?php 
+                if($user->gender == 'f') {
+                    echo 'Female';
+                } else {
+                    echo 'Male';
+                }?></td>
+     </tr>
+     <tr class='odd '>
+            <th>Address:</th>
+            <td><?php echo $user->address; ?></td>
+     </tr>
+    </tbody>
+</table>
